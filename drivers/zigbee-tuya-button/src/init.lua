7d48cfedb7ceeb32cb31a0140122daae67adeaf3
zambobmaz <zambobmaz@gmail.com>
2021-10-03 21:19:50 +0900

    init

--- drivers/zigbee-tuya-button/src/init.lua
+++ drivers/zigbee-tuya-button/src/init.lua
@@ -21,16 +21,32 @@ local zcl_clusters = require "st.zigbee.zcl.clusters"
 function button_handler (driver, device, zb_rx)
     log.info("--------- Moon --------->> button_handler")
 
-    local clickType = string.byte(zb_rx.body.zcl_body.body_bytes) -- 00: click, 01: double click, 02: hold_release
     local ep = zb_rx.address_header.src_endpoint.value
-
     local component_id = string.format("button%d", ep)
 
     if ep == 1 then
         component_id = "main"
     end
 
-    device.profile.components[component_id]:emit_event(capabilities.button.button.pushed())
+    -- 00: click, 01: double click, 02: hold_release
+    local clickType = string.byte(zb_rx.body.zcl_body.body_bytes)
+    if clickType == 0 then
+        local ev = capabilities.button.button.pushed()
+        ev.state_change = true
+        device.profile.components[component_id]:emit_event(ev)
+    end
+
+    if clickType == 1 then
+        local ev = capabilities.button.button.double()
+        ev.state_change = true
+        device.profile.components[component_id]:emit_event(ev)
+    end
+
+    if clickType == 2 then
+        local ev = capabilities.button.button.held()
+        ev.state_change = true
+        device.profile.components[component_id]:emit_event(ev)
+    end
 end
 
 local device_added = function(driver, device)
@@ -49,11 +65,11 @@ local configure_device = function(self, device)
     device:send(zcl_clusters.PowerConfiguration.attributes.BatteryPercentageRemaining:read(device))
 end
 
-local device_init = function(self, device)
-    log.info("--------- Moon --------->> device_init")
-    device:set_component_to_endpoint_fn(component_to_endpoint)
-    device:set_endpoint_to_component_fn(endpoint_to_component)
-end
+--local device_init = function(self, device)
+--    log.info("--------- Moon --------->> device_init")
+--    device:set_component_to_endpoint_fn(component_to_endpoint)
+--    device:set_endpoint_to_component_fn(endpoint_to_component)
+--end
 
 local zigbee_tuya_button_driver_template = {
     supported_capabilities = {
@@ -74,7 +90,7 @@ local zigbee_tuya_button_driver_template = {
     lifecycle_handlers = {
         added = device_added,
         doConfigure = configure_device,
-        init = device_init,
+        --init = device_init,
     }
 }
 
