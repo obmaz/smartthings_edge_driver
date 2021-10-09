17311545539072c08e259abe09a06006f6a944a9
zambobmaz <zambobmaz@gmail.com>
2021-10-09 21:26:29 +0900

    init

--- drivers/zigbee-tuya-switch/src/init.lua
+++ drivers/zigbee-tuya-switch/src/init.lua
@@ -73,31 +73,38 @@ local off_handler = function(driver, device, command)
     device:send_to_component(command.component, zcl_clusters.OnOff.server.commands.Off(device))
 end
 
+local received_handler = function(driver, device, value)
+    log.info("--------- Moon --------->> received_handler", value.OnOff)
+
+    local ep = value.address_header.src_endpoint.value
+    local component_id = string.format("switch%d", ep)
+
+    -- true / false
+    local clickType = string.byte(value.body.zcl_body.body_bytes)
+
+    local ev = capabilities.switch.switch.off()
+    if clickType == true then
+        ev = capabilities.switch.switch.on()
+    end
+
+    ev.state_change = true
+    if component_id == getRemapSwitch(device) then
+        device.profile.components["main"]:emit_event(ev)
+    end
+
+    device.profile.components[component_id]:emit_event(ev)
+end
+
 local component_to_endpoint = function(device, component_id)
     log.info("--------- Moon --------->> component_to_endpoint - component_id : ", component_id)
     local ep = component_id:match("switch(%d)")
     return ep and tonumber(ep) or device.fingerprinted_endpoint_id
 end
 
--- 물리 버튼을 누를때는 명시적, 아닐 경우 주기적으로 기기가 리포팅함
+-- received_handler를 사용해서 이건 호출되지 않음
 local endpoint_to_component = function(device, ep)
     log.info("--------- Moon --------->> endpoint_to_component - endpoint : ", ep)
-
     local component_id = string.format("switch%d", ep)
-
-    if getRemapSwitch(device) == component_id and isAlreadyEmit == false then
-        -- 버튼을 눌렀을때와 아닐때 구분 필요
-        -- reportAttribute로 다르기는 함 readAttribute
-        -- 컴스텀 핸들러로 만들어야 할듯
-        --syncComponent(device, "on")
-    end
-
-    isAlreadyEmit = false
-
-    if getRemapSwitch(device) == "main" then
-        component_id = "main"
-    end
-
     return component_id
 end
 
@@ -140,10 +147,6 @@ local function configure_device(self, device)
     device:configure()
 end
 
---function button_handler (driver, device, zb_rx)
---    log.info("--------- Moon --------->> button_handler")
---end
-
 local zigbee_tuya_switch_driver_template = {
     supported_capabilities = {
         capabilities.switch,
@@ -155,15 +158,13 @@ local zigbee_tuya_switch_driver_template = {
             [capabilities.switch.commands.off.NAME] = off_handler
         }
     },
-    --zigbee_handlers = {
-    --    cluster = {
-    --        -- zcl_clusters.OnOff.server.commands.OnOff.ID
-    --        [0x0006] = {
-    --            -- ZCLCommandId
-    --            [0x0A] = button_handler
-    --        }
-    --    }
-    --},
+    zigbee_handlers = {
+        attr = {
+            [zcl_clusters.OnOff.ID] = {
+                [zcl_clusters.OnOff.attributes.OnOff.ID] = received_handler
+            }
+        }
+    },
     lifecycle_handlers = {
         infoChanged = device_info_changed,
         init = device_init,
@@ -177,11 +178,3 @@ defaults.register_for_default_handlers(zigbee_tuya_switch_driver_template, zigbe
 local zigbee_driver = ZigbeeDriver("zigbee-tuya-switch", zigbee_tuya_switch_driver_template)
 --local zigbee_driver = ZigbeeDriver("zigbee-tuya-switch-dev", zigbee_tuya_switch_driver_template)
 zigbee_driver:run()
-
---        <ZigbeeDevice: 9a334f24-44fc-42a2-bae3-7e96ef58ad9c [0xFBFA] (침실 조명)> received Zigbee message: < ZigbeeMessageRx || type: 0x00, < AddressHeader || src_addr: 0xFBFA, src
---_endpoint: 0x01, dest_addr: 0x0000, dest_endpoint: 0x01, profile: 0x0104, cluster: OnOff >, lqi: 0xFF, rssi: -60, body_length: 0x0008, < ZCLMessageBody || < ZCLHeader || frame_ctrl: 0x18, seqno: 0x37, ZCLCommandId: 0x01 >, < ReadAttr
---ibuteReponse || < AttributeRecord || AttributeId: 0x0000, ZclStatus: SUCCESS, DataType: Boolean, OnOff: false > > > >
-
---        <ZigbeeDevice: 9a334f24-44fc-42a2-bae3-7e96ef58ad9c [0xFBFA] (침실 조명)> received Zigbee message: < ZigbeeMessageRx || type: 0x00, < AddressHeader || src_addr: 0xFBFA, src
---_endpoint: 0x01, dest_addr: 0x0000, dest_endpoint: 0x01, profile: 0x0104, cluster: OnOff >, lqi: 0xFF, rssi: -70, body_length: 0x0007, < ZCLMessageBody || < ZCLHeader || frame_ctrl: 0x08, seqno: 0x3E, ZCLCommandId: 0x0A >, < ReportAt
---tribute || < AttributeRecord || AttributeId: 0x0000, DataType: Boolean, OnOff: true > > > >
