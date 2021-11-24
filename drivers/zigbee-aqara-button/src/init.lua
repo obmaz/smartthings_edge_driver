62af05e2a9b45dd781b83696d6c433981d1403e9
zambobmaz <zambobmaz@gmail.com>
2021-11-24 22:58:09 +0900

    Fixed

---
+++ drivers/zigbee-aqara-button/src/init.lua
@@ -0,0 +1,90 @@
+-- Zigbee Tuya Button
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+
+local log = require "log"
+local capabilities = require "st.capabilities"
+local ZigbeeDriver = require "st.zigbee"
+local defaults = require "st.zigbee.defaults"
+local zcl_clusters = require "st.zigbee.zcl.clusters"
+
+function button_handler (driver, device, zb_rx)
+  log.info("<<---- Moon ---->> button_handler")
+
+  local ep = zb_rx.address_header.src_endpoint.value
+  -- ToDo: Check logic when end_point is not 0x01
+  local component_id = string.format("button%d", ep)
+
+  -- 00: click, 01: double click, 02: hold_release
+  local clickType = string.byte(zb_rx.body.zcl_body.body_bytes)
+  if clickType == 0 then
+    local ev = capabilities.button.button.pushed()
+    ev.state_change = true
+    device.profile.components[component_id]:emit_event(ev)
+  end
+
+  if clickType == 1 then
+    local ev = capabilities.button.button.double()
+    ev.state_change = true
+    device.profile.components[component_id]:emit_event(ev)
+  end
+
+  if clickType == 2 then
+    local ev = capabilities.button.button.held()
+    ev.state_change = true
+    device.profile.components[component_id]:emit_event(ev)
+  end
+end
+
+local device_added = function(driver, device)
+  log.info("<<---- Moon ---->> device_added")
+
+  for key, value in pairs(device.profile.components) do
+    log.info("<<---- Moon ---->> device_added - component : ", key)
+    device.profile.components[key]:emit_event(capabilities.button.supportedButtonValues({ "pushed", "double", "held" }))
+    device.profile.components[key]:emit_event(capabilities.button.button.pushed())
+  end
+end
+
+local device_info_changed = function(driver, device, event, args)
+  -- workaround : edge driver bug..sometime device lost own supported button
+  for key, value in pairs(device.profile.components) do
+    log.info("<<---- Moon ---->> device_added - component : ", key)
+    device.profile.components[key]:emit_event(capabilities.button.supportedButtonValues({ "pushed", "double", "held" }))
+  end
+end
+
+local zigbee_tuya_button_driver_template = {
+  supported_capabilities = {
+    capabilities.button,
+    capabilities.battery,
+    capabilities.refresh
+  },
+  zigbee_handlers = {
+    cluster = {
+      -- No Attr Data from zb_rx, so it should use cluster handler
+      [zcl_clusters.OnOff.ID] = {
+        -- ZCLCommandId
+        [0xFD] = button_handler
+      }
+    },
+  },
+  lifecycle_handlers = {
+    added = device_added,
+    infoChanged = device_info_changed,
+  }
+}
+
+defaults.register_for_default_handlers(zigbee_tuya_button_driver_template, zigbee_tuya_button_driver_template.supported_capabilities)
+local zigbee_driver = ZigbeeDriver("zigbee-tuya-button", zigbee_tuya_button_driver_template)
+zigbee_driver:run()
---
+++ drivers/zigbee-sonoff-button/README.md
@@ -0,0 +1,12 @@
+# Edge Driver: Zigbee Sonoff Button
+It is the SmartThings Edge Driver for zigbee sonoff button.  
+It supports 1 buttons.  
+
+## Device UI Screen
+![ui](./readme_images/app1.jpg)
+
+## Support Device
+See fingerprint.yml in src
+
+## License
+It is released under the Apache 2.0 License.
