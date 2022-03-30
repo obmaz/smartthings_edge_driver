f8648254a5d689d16320b740b82b0f3d1e852f0e
zambobmaz <zambobmaz@gmail.com>
2022-03-30 17:39:45 +0900

    Fixed

--- drivers/zigbee-tuya-switch/src/multi-switch/init.lua
+++ drivers/zigbee-tuya-switch/src/multi-switch/init.lua
@@ -211,6 +211,8 @@ local ZIGBEE_TUYA_SWITCH_MULTI_FINGERPRINTS = {
   { mfr = "_TZ3000_wu0shw0i", model = "TS0013" },
   { mfr = "FeiBit", model = "FNB56-ZSW03LX2.0" },
   { mfr = "_TYZB01_mqel1whf", model = "TS0013" },
+  { mfr = "_TYZB01_8gqspaab", model = "TS0003" },
+  { mfr = "FeiBit", model = "FNB56-ZSW02LX2.0" },
 }
 
 local is_multi_switch = function(opts, driver, device)
